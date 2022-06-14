adf10f461b6abb9f7cce776ae8317d2cd5ffd52b
Mayara A <mayara.arj@gmail.com>
2022-06-14 17:16:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace aula10_exercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario1 = new Usuario();
+            Console.WriteLine("Qual é o seu nome?");
+            usuario1.nomeDoUsuario = Console.ReadLine();
+            Console.WriteLine("Qual a sua idade?");
+            usuario1.idadeDoUsuario = int.Parse(Console.ReadLine());
+            usuario1.anoAtual = 2022;
+            usuario1.anoNascimento = usuario1.anoAtual - usuario1.idadeDoUsuario;
+            Console.Write($"Olá {usuario1.nomeDoUsuario}, você nasceu em {usuario1.anoNascimento}!");
+
+        }
+    }
+
+
+    public class Usuario
+    {
+        public string nomeDoUsuario;
+        public int idadeDoUsuario;
+        public int anoAtual;
+        public int anoNascimento;
+
+    }
+}
